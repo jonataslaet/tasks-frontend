74a655c5d54974510e21f7f553e8c4e5954f30d5
jonatas.laet <jonataslaetprogramador@gmail.com>
2022-12-04 16:46:25 -0300

    Remoção de espaços em branco

--- src/main/java/br/ce/wcaquino/tasksfrontend/controller/TasksController.java
+++ src/main/java/br/ce/wcaquino/tasksfrontend/controller/TasksController.java
@@ -3,7 +3,6 @@ package br.ce.wcaquino.tasksfrontend.controller;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
-
 import br.ce.wcaquino.tasksfrontend.model.Todo;
 
 @Controller
